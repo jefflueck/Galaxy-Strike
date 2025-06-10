033a460494b53898b69cbc4a70eb060bacf4d7fb
jefflueck <lueckcfd@yahoo.com>
2025-06-09 19:08:45 -0500

    added weapon script and assigned key in input system, added new material for laser, added laser behavior in unity editor.

---
+++ Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    [SerializeField]
+    GameObject laser;
+    bool isFiring = false;
+
+    void Update()
+    {
+        ProcessFiring();
+    }
+    public void OnFire(InputValue value)
+    {
+
+        isFiring = value.isPressed;
+    }
+
+    private void ProcessFiring()
+    {
+        var emissionModule = laser.GetComponent<ParticleSystem>().emission;
+        emissionModule.enabled = isFiring;
+    }
+
+}
